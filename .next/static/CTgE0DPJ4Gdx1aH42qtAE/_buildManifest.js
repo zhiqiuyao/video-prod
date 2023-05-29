c6d58f79262da5c0fb6a9b17b245ab14e2c82b9c
yaozhiqiu <zhiqiuyao@gmail.com>
2023-05-29 19:59:52 +0800

    2

---
+++ .next/static/CTgE0DPJ4Gdx1aH42qtAE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/213-c3a2ddb343a634a0.js","static/css/d8bc364a04c2ae61.css","static/chunks/pages/index-2c2aa1b886890a9c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
